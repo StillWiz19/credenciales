4ad308df81ff16bed8fffc000f58d33d4968062e
MarcoA <marco.aguilar@inacapmail.cl>
2024-03-06 11:25:58 -0300

    fix

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="styles.css">
     </head>
     <body>
-        <div class="menu"
+        <div class="menu">
         <h1>Gestor de Credenciales</h1>
         <ul>
             <li><a href="registrar.php">Registrar Credenciales</a></li>
